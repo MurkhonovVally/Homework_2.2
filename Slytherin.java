c3403bd13ac31f21de463416c3ffa15e3cbe19c9
Vally <murkhonovbatoha@gmail.com>
2023-02-22 20:57:43 +0800

    Скелет

---
+++ Slytherin.java
@@ -0,0 +1,66 @@
+public class Slytherin extends HogwartsStudent {
+    private int cunning;
+    private int determination;
+    private int ambition;
+    private int resourcefulness;
+    private int lustForPower;
+    public Slytherin(int power, int transgression, int cunning, int determination, int ambition,
+                     int resourcefulness, int lustForPower) {
+        this.cunning = cunning;
+        this.determination = determination;
+        this.ambition = ambition;
+        this.resourcefulness = resourcefulnesse;
+        this.lustForPower = lustForPower;
+    }
+
+    public int getCunning() {
+        return cunning;
+    }
+
+    public int getDetermination() {
+        return determination;
+    }
+
+    public int getAmbition() {
+        return ambition;
+    }
+
+    public int getResourcefulness() {
+        return resourcefulness;
+    }
+
+    public int getLustForPower() {
+        return lustForPower;
+    }
+
+    public void setCunning(int cunning) {
+        this.cunning = cunning;
+    }
+
+    public void setDetermination(int determination) {
+        this.determination = determination;
+    }
+
+    public void setAmbition(int ambition) {
+        this.ambition = ambition;
+    }
+
+    public void setResourcefulness(int resourcefulness) {
+        this.resourcefulness = resourcefulness;
+    }
+
+    public void setLustForPower(int lustForPower) {
+        this.lustForPower = lustForPower;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Slytherin{" +
+                "cunning=" + cunning +
+                ", determination=" + determination +
+                ", ambition=" + ambition +
+                ", resourcefulness=" + resourcefulness +
+                ", lustForPower=" + lustForPower +
+                '}';
+    }
+}
