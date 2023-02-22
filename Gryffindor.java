04d06f031085939a3dca1c98ed6862226087d082
Vally <murkhonovbatoha@gmail.com>
2023-02-22 19:34:17 +0800

    Body

---
+++ Gryffindor.java
@@ -0,0 +1,33 @@
+public class Gryffindor extends HogwartsStudent {
+    private int nobility;
+    private int bravery;
+    private int honor;
+    public Gryffindor(int power, int transgression, int nobility, int bravery, int honor) {
+        super(power, transgression);
+        this.nobility = nobility;
+        this.bravery = bravery;
+        this.honor = honor;
+    }
+    public int getNobility() {
+        return nobility;
+    }
+    public int getBravery() {
+        return bravery;
+    }
+    public int getHonor() {
+        return honor;
+    }
+    public void setNobility(int nobility) {
+        this.nobility = nobility;
+    }
+    public void setBravery(int bravery) {
+        this.bravery = bravery;
+    }
+    public void setHonor(int honor) {
+        this.honor = honor;
+    }
+
+    public java.lang.String toString() {
+        return "Gryffindor{" + "nobility=" + nobility + ", bravery=" + bravery + ", honor=" + honor + '}';
+    }
+}
